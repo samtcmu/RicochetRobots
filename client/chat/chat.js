569352dda3b99faf16cd1029d3cb289001cbedc0
Sam Tetruashvili <samt@alumni.cmu.edu>
2020-04-18 16:31:27 -0400

    Added the ability to store chat history on the server.

--- client/chat/chat.js
+++ client/chat/chat.js
@@ -11,14 +11,24 @@ function sendMessage(event) {
     return false;
 }
 
+function addMessage(message) {
+    const messages = document.getElementById("messages");
+    const messageListItem = document.createElement("li");
+    messageListItem.innerHTML = message;
+    messages.appendChild(messageListItem);
+}
+
+function processHistory(history) {
+    const chats = JSON.parse(history);
+    for (let i = 0; i < chats.length; ++i) {
+        addMessage(chats[i]);
+    }
+}
+
 function loadChatApp() {
     const form = document.getElementById("message-form");
     form.onsubmit = sendMessage;
 
-    socket.on('chat message', function(message){
-        const messages = document.getElementById("messages");
-        const messageListItem = document.createElement("li");
-        messageListItem.innerHTML = message;
-        messages.appendChild(messageListItem);
-    });
+    socket.on('chat message', addMessage);
+    socket.on('chat history', processHistory);
 }
--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
-const path = require('path');
-const express = require('express');
-const socket_io = require('socket.io')
+const path = require("path");
+const express = require("express");
+const socket_io = require("socket.io")
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/chat/index.html'));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/chat/index.html"));
 });
 
-app.use(express.static(path.join(__dirname, '../client/chat')));
+app.use(express.static(path.join(__dirname, "../client/chat")));
 
 const port = process.argv[2]
 const server = app.listen(port, function () {
@@ -21,10 +21,15 @@ const server = app.listen(port, function () {
 // Create the websocket server.
 const io = socket_io(server);
 
-io.on('connection', (socket) => {
+const chats = []
+
+io.on("connection", (socket) => {
     // When receiving a chat message broadcast to all other peers connect to
     // this websocket server.
-    socket.on('chat message', (message) => {
-        io.emit('chat message', message);
+    socket.on("chat message", (message) => {
+        chats.push(message);
+        io.emit("chat message", message);
     });
+
+    io.to(socket.id).emit("chat history", JSON.stringify(chats));
 });
